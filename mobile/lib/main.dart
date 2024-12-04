c5ed72bbc5740c7d01121317556a82ff25e5cf27
zgeniuscoders <zgeniuscoders@gmail.com>
2024-12-04 16:29:17 +0100

    Ameliorationn den l'interface d'acceuille

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -69,6 +69,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Conteur Lumaka',
       localizationsDelegates: [
         FFLocalizationsDelegate(),
